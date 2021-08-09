3e383c598fd4e7fea7531a2632898c8961942e7a
Abdifatah Hassan <abdi.hassannoor@onedayonly.co.za>
2021-08-09 19:20:22 +0200

    adding form inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+import { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const handleClick = () => {
+    setTitle('Tv Show')
+    console.log("axxsd");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={handleClick}>Change title</button>
       </div>
     </Card>
-  );
+  ); 
 };
 
 export default ExpenseItem;

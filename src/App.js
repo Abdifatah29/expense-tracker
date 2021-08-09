af45f066cbb16f421d9175b63f34a66449780109
Abdifatah Hassan <abdi.hassannoor@onedayonly.co.za>
2021-08-09 15:31:31 +0200

    passing data via props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", date: new Date(2021, 10, 29), amount: 294.67 },
+    { title: "Toilet Paper", date: new Date(2021, 6, 17), amount: 294.67 },
+    { title: "Football Ticket", date: new Date(2021, 4, 11), amount: 450 },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      {Object.keys(expenses).map((items, key) => {
+        return (
+          <ExpenseItem
+            key={key}
+            title={expenses[items].title}
+            amount={expenses[items].amount}
+            date={expenses[items].date}
+          />
+        );
+      })}
     </div>
   );
 }
